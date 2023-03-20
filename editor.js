2ce07e7122350929ca0457297d60d8a2f0b203ee
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-03-20 08:59:37 +0100

    Create editor.js

---
+++ editor.js
@@ -0,0 +1,15 @@
+// Registers workers in a serviceWorker Cache then loads editor
+
+// Main Concepts the editor is a range content parser that exposes events via streams.
+// so its a abstraction of the concept of range requests.
+// you can consume the streams to manipulate the editor view as you need as also scroll the content range.
+
+import { editor } from 'components/monaco-editor-core'; // monaco without language support
+// import { editor } from 'components/monaco-editor'; // monaco with monarch languages and vscode languageService (server) support nls new language services
+// import { editor } from 'components/monaco-editor-textmate'; // extra added support for integration of textmate languages via vscode-omnigurma
+// 
+
+// ReadableStream({ read(e) { e.enqueue(editor.create(el)) } })
+// .pipeThrough(new TransformStream({ transform(c,el) { /** do applyLogic */ } }))
+// .pipeTo(new WriteableStream({ write(el) { /** do  Observation something */ } }));
+// 
